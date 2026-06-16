2fbed0fa9dfecc8fc882d6d1c47c6a50e905c86d
mariahelloisa-cyber <maria.helloisa@facla.edu.br>
2026-06-16 09:56:52 -0300

    atualização do projeto

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ document.querySelectorAll('.faq-question').forEach(button => {
         faqItem.classList.toggle('active');
     });
 });
+/* ==========================================================================
+   LIMPAR FORMULÁRIO APÓS O ENVIO (SEM ATUALIZAR A PÁGINA)
+   ========================================================================== */
+const formulario = document.querySelector('.custom-form');
+
+if (formulario) {
+    formulario.addEventListener('submit', () => {
+        // Aguarda um milissegundo para o formulário disparar os dados e limpa os campos
+        setTimeout(() => {
+            formulario.reset();
+            alert('Sua aplicação foi enviada com sucesso!'); 
+        }, 100);
+    });
+}
